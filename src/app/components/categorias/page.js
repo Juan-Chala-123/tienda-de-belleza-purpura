1a48ea588c6daeb7d972ca5ff5e634b12b170cc7
JuanP <jpchalaramirez@gmail.com>
2025-11-30 17:43:26 -0500

    Solucion de error src

--- src/app/components/categorias/page.js
+++ src/app/components/categorias/page.js
@@ -4,19 +4,19 @@ import styles from './images-categories.css';
 const images_categories = [
     {
         page: '/categorias/cuidado-de-la-piel',
-        src: '', alt: 'Cuidado de la piel'
+        src: null, alt: 'Cuidado de la piel'
     },
     {
         page: '/categorias/maquillaje',
-        src: 'null', alt: 'Maquillaje'
+        src: null, alt: 'Maquillaje'
     },
     {
         page: '/categorias/cabello',
-        src: 'null', alt: 'Cabello'
+        src: null, alt: 'Cabello'
     },
     {
         page: '/categorias/accesorios',
-        src: 'null', alt: 'Accesorios'
+        src: null, alt: 'Accesorios'
     }
 ]
 
@@ -26,11 +26,10 @@ export default function Categories_Img () {
             {
                 images_categories.map(({src, alt, page}, index) => (
                     <div className="image" key={index}>
-                        <a href={page}><Image src={src} alt={alt} width={250} height={200} /></a>
+                        <a href={`/${page}`}><Image src={src} alt={alt} width={250} height={200} /></a>
                     </div>
                 ))
             }
         </div>
-
     )
 }
