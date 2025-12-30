7c9ee7d3ccdb9d971797f04a3d67e6bec7d75fcf
JuanP <jpchalaramirez@gmail.com>
2025-12-30 10:02:15 -0500

    Modified files to dockerize the project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {},
+  webpack: (config) => {
+    return config;
+  },
+};
 
 export default nextConfig;
