6fba99b4c00787c4c1a678b5414e42de92518c8e
JuanP <jpchalaramirez@gmail.com>
2026-01-05 13:09:13 -0500

    Modified

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,7 @@
 import { Pool } from 'pg';
 
-const pool = new Pool ({
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  database: process.env.PGDATABASE,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
+
+export default pool;
